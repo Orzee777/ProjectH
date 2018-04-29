53c4682ad9563a5dfb7477b1bdb21845bb598322
Brad <Orzee777>
2018-04-29 02:36:19 -0400

    Added save file, form file, and minor other changes

--- events.js
+++ events.js
@@ -39,7 +39,41 @@ function submit(id) {
 	else if (id === "maybe") {
 		alert("Call me?");
 	}
+	else if (id === "close") {
+		alert("Sorry, no longer open!");
+		close();
+	}
 	else {
 		alert("Link broken :(");
+	}
+}
+
+// Name and Email validation Function.
+function validation(){
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+//var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+
+if( name ==='' || email ===''){
+	alert("Please fill all fields...!!!!!!");
+	return false;}
+/*else if(!(email).match(emailReg)){
+	alert("Invalid Email...!!!!!!");
+	return false;} */
+else{
+	return true;
+	}
+}
+
+function getForm() {
+	if (validation()) {
+		var name = document.getElementById("name").value;
+		var email = document.getElementById("email").value;
+		var x = document.getElementsByName("form_name");
+		x[0].submit();
+		alert("Name: " + name + "\nEmail: " + email + "\nForm Class: " + document.getElementById("form_id").getAttribute("class") + "\n\nForm Submitted Successfully......");
+	}
+	else {
+		alert("Improper Inputs!");
 	}
 }
