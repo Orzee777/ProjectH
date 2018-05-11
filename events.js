5d04ff9acf292ff30a7a23cbda2f691abef7a828
Brad <Orzee777>
2018-05-11 12:38:11 -0400

    Created new form submission functions

--- events.js
+++ events.js
@@ -2,8 +2,10 @@
 
 var checklist = document.getElementById("names");
 
-var items = checklist.querySelectorAll("li");
-var inputs = checklist.querySelectorAll("input");
+//var items = checklist.querySelectorAll("li");
+//var inputs = checklist.querySelectorAll("input");
+
+var items = [];
 
 for (var i = 0; i < items.length; i++) {
   items[i].addEventListener("click", editItem);
@@ -46,66 +48,4 @@ function submit(id) {
 	else {
 		alert("Link broken :(");
 	}
-}
-/*
-// Name and Email validation Function.
-function validation(){
-var name = document.getElementById("name").value;
-var email = document.getElementById("email").value;
-//var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
-
-if(name ==='' || email ===''){
-	alert("Please fill all fields...!!!!!!");
-	return false;}
-/*else if(!(email).match(emailReg)){
-	alert("Invalid Email...!!!!!!");
-	return false;} 
-else{
-	return true;
-	}
-}
-*/
-function validation() {
-	// Create a new object inputElement
-	function inputElement(name, newValue) {
-	this.name = name;
-	this.newValue = newValue;
-}
-	
-	var name = new inputElement("name", document.getElementById("name").value);
-	var email = new inputElement("email", document.getElementById("email").value);
-	
-	var formlist = [name, email];
-	
-	for (i = 0; i < formlist.length; i++) {
-		if (!formlist[i].newValue) {
-			alert("Please enter your " + formlist[i].name);
-			return false;
-		}
-	}
-	return true;
-}
-
-function getForm() {
-	if (validation()) {
-		var name = document.getElementById("name").value;
-		var email = document.getElementById("email").value;
-		var x = document.getElementsByName("form_name");
-		x[0].submit();
-		alert("Name: " + name + "\nEmail: " + email + "\nForm Class: " + document.getElementById("form_id").getAttribute("class") + "\n\nForm Submitted Successfully......");
-		replaceText("NameInput", name);
-		addText("EmailInput", getText("EmailInput"), email);
-	}
-}
-
-function getText(id) {
-	return document.getElementById(id).innerHTML;
-}
-
-function replaceText(id, value) {
-	document.getElementById(id).innerHTML = value;
-}
-
-function addText(id, old, value) {
-	document.getElementById(id).innerHTML = old + "<br>" + value;
 }
---
+++ scripts/forms.js
@@ -0,0 +1,89 @@
+// Javascript Forms File
+
+function editItem() {
+  this.className = "edit";
+  var input = this.querySelector("input");
+  input.focus();
+  input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+  this.previousElementSibling.innerHTML = this.value;
+  this.parentNode.className = "";
+}
+
+function itemKeypress(event) {
+  if (event.which === 13) {
+    updateItem.call(this);
+  }
+}
+
+function submit(id) {
+	if (id === "cancel") {
+		alert("Cancelled");
+	}
+	else if (id === "submit") {
+		alert("Data Submitted");
+	}
+	else if (id === "maybe") {
+		alert("Call me?");
+	}
+	else if (id === "close") {
+		alert("Sorry, no longer open!");
+		close();
+	}
+	else {
+		alert("Link broken :(");
+	}
+}
+
+function validation() {
+	// Create a new object inputElement
+	function inputElement(name, newValue) {
+	this.name = name;
+	this.newValue = newValue;
+}
+	
+	var name = new inputElement("name", document.getElementById("name").value);
+	var email = new inputElement("email", document.getElementById("email").value);
+	var password = new inputElement("password", document.getElementById("password").value);
+	
+	var formlist = [name, email, password];
+	
+	for (i = 0; i < formlist.length; i++) {
+		if (!formlist[i].newValue) {
+			alert("Please enter your " + formlist[i].name);
+			return false;
+		}
+	}
+	return true;
+}
+
+function getForm() {
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	alert("Name: " + name + "\nEmail: " + email + "\nPassword: " + password + "\nForm Class: " + document.getElementById("form_id").getAttribute("class") + "\nForm Submitted Successfully!");
+
+	document.getElementById("NameInput").innerHTML = name;
+	document.getElementById("EmailInput").innerHTML = email;
+	document.getElementById("Password").innerHTML = password;
+}
+/*
+function getText(id) {
+	console.log("getText ran");
+	console.log(document.getElementById(id).innerHTML);
+	return document.getElementById(id).innerHTML;
+}
+
+function replaceText(id, value) {
+	console.log("replaceText ran");
+	document.getElementById(id).innerHTML = value;
+}
+
+function addText(id, old, value) {
+	console.log("addText ran");
+	document.getElementById(id).innerHTML = old + "\n" + value;
+}
+*/
