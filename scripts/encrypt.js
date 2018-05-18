33ca964568c3d626ac6957cd309ff22c2626ce31
Brad <Orzee777>
2018-05-18 14:42:18 -0400

    added prototype versions of functions

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -41,6 +41,7 @@ var encrypt = function (unencrypted) {
 	return encrypted;
 };
 
+
 // function 'decrypt'
 //   decrypts str 'unencrypted' using the array 'alphabet'
 var decrypt = function (encrypted) {
@@ -66,3 +67,46 @@ var decrypt = function (encrypted) {
 var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 	"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 	" ","0","1","2","3","4","5","6","7","8","9"];
+
+
+// prototype 'encrypt'
+//   prototype version of function 'encrypt'
+String.prototype.encrypt = function () {
+
+	var encrypted = "";
+	
+	for (unencryptedLetter = 0; unencryptedLetter < this.length; unencryptedLetter++) {
+
+		i = find(alphabet, this[unencryptedLetter]) + 15;
+
+		if (i >= alphabet.length) {
+			i -= alphabet.length;
+		}
+
+		encrypted += alphabet[i];
+
+	}
+
+	return encrypted;
+};
+
+// prototype 'decrypt'
+//   prototype version of function 'decrypt'
+String.prototype.decrypt = function () {
+
+	var decrypted = "";
+
+	for (encryptedLetter = 0; encryptedLetter < this.length; encryptedLetter++) {
+
+		j = find(alphabet, this[encryptedLetter]) - 15;
+
+		if (j < 0) {
+			j += alphabet.length;
+		}
+
+		decrypted += alphabet[j];
+
+	}
+
+	return decrypted;
+};
