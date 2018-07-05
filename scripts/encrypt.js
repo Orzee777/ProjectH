a201a80b1ee045c49a799a5d4315ef0904a1e403
Orzee777 <37989885+Orzee777@users.noreply.github.com>
2018-07-05 14:24:18 -0400

    updated Alphabet and localStorage functions
    alphabet now includes symbols
    can now effectively utilize localStorage for future projects

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -63,10 +63,31 @@ var decrypt = function (encrypted) {
 	return decrypted;
 };
 
-// alphabet currently doesn't support symbols, just alphanumerical characters
-var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-	"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-	" ","0","1","2","3","4","5","6","7","8","9"];
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","0","1","2","3","4","5","6","7","8","9",".",",","!","@","#","$","%","^","&","*","(",")","-","_","+","=","\'","?","<",">","[","]","{","}","/",":",":","|","\`","~"];
+// needs to be renamed
+// encrypts a message in element TBD
+// saves element at "saved", which is a temporary placeholder
+var encryptAndSend = function () {
+  message = document.getElementById("message").value;
+  newMessage = encrypt(message);
+  document.getElementById("log").innerHTML = newMessage;
+  localStorage.setItem("saved", newMessage);
+}
+
+// needs to be renamed
+// decrypts message in "saved" from encryptAndSend
+// and displays it in the log
+var decryptAndSend = function () {
+  message = localStorage.getItem("saved");
+  if (message != null) {
+  //message = document.getElementById("log").innerHTML;
+  newMessage = decrypt(message);
+  document.getElementById("log").innerHTML = newMessage;
+  }
+  else {
+    console.log("ERROR. SAVED COULD NOT BE FOUND");
+  }
+}
 
 
 // prototype 'encrypt'
